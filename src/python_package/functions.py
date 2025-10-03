1dc0e9e363e07cacefc6efd03ced1560cdf3657d
avakeerak <ava.keerak@ucalgary.ca>
2025-10-02 19:27:43 -0600

    saved changes to functions.py, arcsin function has been added

--- src/python_package/functions.py
+++ src/python_package/functions.py
@@ -27,7 +27,7 @@ def sqrt(x):
             if k == 1:
                 prod_k = 1
             else:
-                prod_k *= (-0.5 * ((2 * k) - 1))
+                prod_k *= (-0.5 * ((2 * j) - 1))
 
         coef_k = 0.5 * prod_k / fact_k
 
@@ -35,10 +35,41 @@ def sqrt(x):
 
         sum_k += term_k
 
-    return sum_k
+        sqrt_x = sqrt_a + sum_k
+
+    return sqrt_x
 
     #use and if statement to check if x is between 0 and 2.5 --> values outside that range should result in an error
     #use taylow series
 
+def arcsin(x):
+    
+    for n in range(1, 20):
+
+        fact_2n = 1
+
+        for i in range(1, n + 1):
+            fact_2n *= 2 * i
+
+        fact_n = 1
+
+        for j in range(1, n + 1):
+            fact_n *= j
+
+        denom_n = (n ** 2) * (fact_2n / (fact_n **2))
+
+        numer_n = (2 * x) ** (2 * n)
+
+        term_n = numer_n / denom_n
+
+        sum_n += term_n
+
+        arcsin_x = sum_n * 0.5
+
+    return arcsin_x
+
 # series only converges for a limeted range of values, so output is from 0 to sqrt(2.5). T series will not converge at every point, so we can use base points.
 # hard code in these base values for a. Ex. from 0 to 0.75, 0.5 is used (put into 0.5 bins). Therefore the distance between x and a is less than 0.5. So a^0.5 = .... for each of the base values
+
+
+    )
